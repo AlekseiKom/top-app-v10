a64417d98d3d279a133fa887482d7616694bc24a
Aleksei Komarov <53940821+AlekseiKom@users.noreply.github.com>
2023-05-24 18:04:21 +0300

    Htag component created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&display=swap" rel="stylesheet" />
     </Head>
 
-    <Component {...pageProps} />;
+    <Component {...pageProps} />
   </>;
 }
 
