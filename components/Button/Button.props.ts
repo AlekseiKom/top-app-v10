6786d16439a06e1470c2f976fdccbac27c5777af
Aleksei Komarov <53940821+AlekseiKom@users.noreply.github.com>
2023-05-26 22:45:30 +0300

    HTMLattributes verified

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,6 +1,6 @@
-import { ReactNode } from 'react';
+import { HTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
 
-export interface ButtonProps {
+export interface ButtonProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
     children: ReactNode;
     appearance: 'primary' | 'ghost';
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag='h1'>Teкст</Htag>
-      <Button appearance='primary'> ТЕСТ </Button>
+      <Button appearance='primary' className='testClass'> ТЕСТ </Button>
       <Button appearance='ghost'> ТЕСТ </Button>
     </div >
   );
